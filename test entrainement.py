38dab2143fd21072c9ae477bb1007d9635d6f9bb
Mousseauliam <158599547+Mousseauliam@users.noreply.github.com>
2024-04-13 18:08:58 +0200

    entrainement dd

--- test entrainement.py
+++ test entrainement.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
  
 # Load the model.
-model = YOLO('yolov8n.pt')
+model = YOLO("runs/detect/v1_pikachu/weights/best.pt")
  
 # Training.
 results = model.train(
    data='pikachu.yaml',
-   imgsz=1280,
+   imgsz=750,
    epochs=50,
    batch=8,
-   name='test pika'
+   name='test pikav1 dd'
 )
