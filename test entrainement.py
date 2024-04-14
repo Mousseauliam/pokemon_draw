ea4041ae475a907e6641460b3792fd9a12eea21d
Mousseauliam <158599547+Mousseauliam@users.noreply.github.com>
2024-04-14 11:16:57 +0200

    pikachu_v2

--- test entrainement.py
+++ test entrainement.py
@@ -4,10 +4,19 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
  
 # Training.
+"""
 results = model.train(
    data='pikachu.yaml',
    imgsz=750,
    epochs=50,
    batch=8,
    name='pikachu_v2'
+)"""
+
+results = model.train(
+   data='tiplouf.yaml',
+   imgsz=750,
+   epochs=50,
+   batch=8,
+   name='ti+pi_v1'
 )
