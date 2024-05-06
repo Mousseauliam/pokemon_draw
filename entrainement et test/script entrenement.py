e64d4d29230efebd56df0bb9bd3399a9a21e2657
Mousseauliam <158599547+Mousseauliam@users.noreply.github.com>
2024-05-06 15:06:18 +0200

    totalv3

--- entrainement et test/script entrenement.py
+++ entrainement et test/script entrenement.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
  
 # Load the model.
-model = YOLO("runs/detect/total_v1/weights/best.pt")
+model = YOLO("yolov8n.pt")
  
 # Training.
 results = model.train(
    data='total.yaml',
    imgsz=750,
-   epochs=50,
+   epochs=200,
    batch=8,
-   name='total_v2'
+   name='total_v3'
 )
